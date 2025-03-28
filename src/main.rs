e45b6a56dc0efa1f08aeba16a07fbc425123763e
martuscellifaria <martuscellifaria@gmail.com>
2025-03-28 16:00:30 +0100

    Added threads for randling multiple connections and messages

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use socket::{Socket, TCPServer};
+use socket::TCPServer;
 mod socket;
 
 fn main() {
 
-    let server = TCPServer::default();
+    let mut server = TCPServer::default();
+    server.initialize_socket("127.0.0.1".to_string(), 5001);
     let _ = server.start_listening();
 
 }
--- src/socket.rs
+++ src/socket.rs
@@ -1,35 +1,7 @@
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
+use std::sync::{Arc, Mutex};
 
-pub struct Socket {
-    address: String,
-    port: u16
-    
-}
-
-impl Default for Socket {
-    fn default() -> Self {
-        Self { address: "localhost".to_string(), port: 8080 }
-    }
-}
-
-impl Socket {
-    pub fn initialize_socket(&mut self, address: String, port: u16) {
-        self.set_address(address);
-        self.set_port(port);
-    }
-    fn set_port(&mut self, port: u16) {
-        self.port = port;
-    }
-    fn set_address(&mut self, address: String) {
-        self.address = address;
-    }
-    pub fn get_port(&mut self) -> u16 {
-        self.port
-    }
-    pub fn get_address(&mut self) -> String {
-        self.address.clone()
-    }
-}
+use std::time::Duration;
 
 pub struct TCPServer {
     address: String,
@@ -40,6 +12,25 @@ impl Default for TCPServer {
     fn default() -> Self {
         Self { address: "0.0.0.0".to_string(), port: 8080 }
     }
+}
+
+fn handle_read(mut stream: TcpStream) {
+    let mut buffer = [0;4096];
+    match stream.read(&mut buffer) {
+        Ok(size) => {
+            let received = String::from_utf8_lossy(&buffer[..size]);
+            println!("Received: {}", received);
+        }
+        Err(e) => {
+            eprintln!("Failed to read from client {}", e);
+        }
+    }
+}
+
+fn handle_write(mut stream: TcpStream, message: String){
+    if let Err(err) = stream.write_all(message.as_bytes()) {
+        println!("{0}", err);
+    };
 }
 
 impl TCPServer {
@@ -60,32 +51,26 @@ impl TCPServer {
         self.address.clone()
     }
 
-    fn handle_client(&self, mut stream: TcpStream) {
-        let mut buffer = [0;4096];
-        match stream.read(&mut buffer) {
-            Ok(size) => {
-                let received = String::from_utf8_lossy(&buffer[..size]);
-                println!("Received: {}", received);
-            }
-            Err(e) => {
-                eprintln!("Failed to read from client {}", e);
-            }
-        }
-    }
-
-    fn write_to_client(&self, mut stream: TcpStream, message: String){
-        if let Err(err) = stream.write_all(message.as_bytes()) {
-            println!("{0}", err);
-        };
-    }
-
-    pub fn start_listening(self)-> std::io::Result<()> {
-        let full_address = self.address.clone() + ":" + &self.port.to_string();
-        println!("{full_address}");
-        let listener = TcpListener::bind(full_address)?;
+    pub fn start_listening(self) {
+        let shared_self = Arc::new(self);
+        let full_address = shared_self.address.clone() + ":" + &shared_self.port.to_string();
+        let listener = TcpListener::bind(full_address).unwrap();
+        let active_threads = Arc::new(Mutex::new(0));
         for stream in listener.incoming() {
-            self.handle_client(stream?);
+            let active_requests=Arc::clone(&active_threads);
+            let stream=stream.unwrap();
+            thread::spawn(move || {
+            {
+                let mut connection=active_requests.lock().unwrap();
+                *connection+=1;
+                if *connection>=5{
+                    thread::sleep(Duration::from_secs(2));
+                }
+            }
+            handle_read(stream);
+            let mut connection=active_requests.lock().unwrap();
+            *connection-=1;
+            });
         }
-        Ok(())
     }
 }
